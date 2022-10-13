3f327f0c7d6f89344af151cad0a42d679c75d68d
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2022-10-13 17:03:41 -0400

    update files
    Updated mathematical function of calculator and recognizing user input. Determined what's the first value, second value, operator, whether user is adding to first value, zero, or last answer.

--- script.js
+++ script.js
@@ -26,28 +26,62 @@ function operate(operator,a,b) {
     }
 }
 
-let expression = [];
-// const buttons = document.querySelectorAll("button");
-const display = document.querySelector("th");
-
-// buttons.forEach((button) => {
-//     button.addEventListener('click',() => {
-//         console.log("hi")
-//         console.log(button.alt);
-//         const selection = button.alt;
-//         expression.push(selection);
-//         display.textContent = selection;
-//     });
-// })
-
 const buttons = document.querySelectorAll("input");
+const display = document.querySelector("th");
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let answer = "";
+
+/**console.log(selection);
+        console.log(expression);= button.value;
+       
+**/
 
 buttons.forEach((button) => {
     button.addEventListener('click',() => {
-        const selection = button.value;
+        if (button.classList.contains("num")) {
+            console.log("num");
+            if (!operator) { //inputting first value
+                firstNum += button.value;
+                console.log(firstNum)
+            } else if (answer && firstNum === "") { //inputting second value to add to the last answer
+                firstNum = answer;
+                answer = "";
+                secondNum += button.value;
+                console.log(firstNum, answer, operator, secondNum);
+            } else { //inputting second value to add to the first value
+                secondNum += button.value;
+                console.log(secondNum)
+            }
+        } else if (button.classList.contains("operator")) {
+            console.log("operator");
+            operator = button.value;
+            console.log(operator);
+        } else if (button.classList.contains("equal")) {
+            console.log("equal");
+            answer = operate(operator,Number(firstNum),Number(secondNum));
+            console.log(answer, firstNum, operator, secondNum);
+            firstNum = "";
+            operator = "";
+            secondNum = "";
+            console.log(answer, firstNum, operator, secondNum);
+            
+        } else if (button.classList.contains("clear")) {
+            console.log("clear");
+            firstNum = "";
+            operator = "";
+            secondNum = "";
+            answer = "";
+            console.log(answer, firstNum, operator, secondNum);
+        } else if (button.classList.contains("sign")) {
+            console.log("sign"); 
+        } else if (button.classList.contains("decimal")) {
+            console.log("decimal"); 
+        }
         expression.push(selection);
-        console.log(selection);
-        console.log(expression);
         display.textContent = selection;
     });
 })
+
+
