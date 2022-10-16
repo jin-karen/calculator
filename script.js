51c4fbe3d2cc6d5ef6a48263d58725a153476b61
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2022-10-16 00:18:09 -0400

    update files
    Updated files to encompass all relevant logic and functionality of calculator

--- script.js
+++ script.js
@@ -11,7 +11,17 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
+    if (b === 0) {
+        return "Error";
+    }
     return a / b;
+}
+
+function modulo(a,b) {
+    if (b === 0) {
+        return "Error";
+    }
+    return a % b;
 }
 
 function operate(operator,a,b) {
@@ -23,6 +33,8 @@ function operate(operator,a,b) {
         return multiply(a,b);
     } else if (operator === "/") {
         return divide(a,b);
+    } else if (operator === "%") {
+        return modulo(a,b);
     }
 }
 
@@ -35,55 +47,69 @@ let answer = "";
 
 /**console.log(selection);
         console.log(expression);= button.value;
-       
+       display.textContent = selection;
 **/
 
 buttons.forEach((button) => {
     button.addEventListener('click',() => {
-        if (button.classList.contains("num")) {
-            console.log("num");
-            if (!operator) { //inputting first value
-                firstNum += button.value;
-                console.log(firstNum)
-            } else if (answer && firstNum === "") { //inputting second value to add to the last answer
-                firstNum = answer;
-                answer = "";
-                secondNum += button.value;
-                console.log(firstNum, answer, operator, secondNum);
-            } else { //inputting second value to add to the first value
-                secondNum += button.value;
-                console.log(secondNum)
-            }
-        } else if (button.classList.contains("operator")) {
-            console.log("operator");
-            if (!secondNum){//to add or change the operator before inputting second value
-                operator = button.value;
-            } else{//to add a second operator to the existing expression
-                evaluate()
-                clear()
-                operator = button.value;
-            }
-            console.log(operator);
-        } else if (button.classList.contains("equal")) {
-            console.log("equal");
-            evaluate();
-            console.log(answer, firstNum, operator, secondNum);
-            clear()
-            console.log(answer, firstNum, operator, secondNum);
-        } else if (button.classList.contains("clear")) {
-            console.log("clear");
-            clear();
-            answer = "";
-            console.log("hi",answer, firstNum, operator, secondNum);
-        } else if (button.classList.contains("sign")) {
-            console.log("sign"); 
-        } else if (button.classList.contains("decimal")) {
-            console.log("decimal"); 
-        }
-        expression.push(selection);
-        display.textContent = selection;
+        useCalculator(button);
     });
 })
+
+//calculator logic
+function useCalculator(button){
+    if (button.classList.contains("num")) {
+        console.log("num");
+        if (!operator) { //inputting first value
+            if (button.value === "." && firstNum.includes(".")) return; 
+            firstNum += button.value;
+            answer = ""
+            console.log(firstNum)
+            display.textContent = firstNum;
+        } else if (answer && firstNum === "") { //inputting second value (to add to the last answer)
+            if (button.value === "." && secondNum.includes(".")) return;
+            firstNum = String(answer);
+            answer = "";
+            secondNum += button.value;
+            display.textContent = secondNum;
+            console.log(firstNum, answer, operator, secondNum);
+        } else { //inputting second value (to add to the first value)
+            if (button.value === "." && secondNum.includes(".")) return;
+            secondNum += button.value;
+            display.textContent = secondNum;
+            console.log(secondNum)
+        }
+    } else if (button.classList.contains("operator")) {
+        console.log("operator");
+        if (!secondNum){//to add or change the operator before inputting second value
+            operator = button.value;
+            display.textContent = operator;
+            unhighlight();
+            button.classList.toggle("highlight");
+        } else{//to add a second operator to the existing two-term expression
+            evaluate()
+            clear()
+            operator = button.value;
+            display.textContent = operator;
+        }
+        console.log(operator);
+    } else if (button.classList.contains("equal")) {
+        console.log("equal");
+        evaluate();
+        display.textContent = answer;
+        console.log(answer, firstNum, operator, secondNum);
+        clear()
+        console.log(answer, firstNum, operator, secondNum);
+    } else if (button.classList.contains("clear")) {
+        console.log("clear");
+        clear();
+        display.textContent = "";
+        answer = "";
+        console.log("hi",answer, firstNum, operator, secondNum);
+    } else if (button.classList.contains("sign")) {
+        negate();
+    } 
+}
 
 //clear all values except answer
 function clear() { 
@@ -94,5 +120,65 @@ function clear() {
 
 //evaluate the mathematical expression
 function evaluate() {
-    answer = operate(operator,Number(firstNum),Number(secondNum));
+    if (firstNum === "-") {
+        firstNum = "";
+    } else if (secondNum === "-") {
+        secondNum ="";
+    } 
+    if (!firstNum) { //if no new values or operator inputted, return last available answer 
+        answer = answer;
+    } else if (!secondNum) { //if no operator or second values inputted, return first value
+        answer = firstNum;
+    } else if (firstNum === "Error") {
+        answer = "Error";
+    } else{
+        answer = (operate(operator,Number(firstNum),Number(secondNum)));
+    }
+    unhighlight()
 }
+
+//unhighlighting any highlighted buttons by toggling class off
+function unhighlight() {
+    const highlighted = document.querySelectorAll(".highlight");
+    for (const buttons of highlighted) {
+        buttons.classList.toggle("highlight");
+    }
+}
+
+//turn positive numbers into negative numbers and vice versa
+function negate() {
+    console.log("sign"); 
+    if (answer && !operator) { //negating answer
+        if (answer === "Error") {
+            return;
+        }
+        answer = String(-answer);
+        console.log(answer);
+        display.textContent = answer;
+    } else if (!firstNum && !operator) { //negating first value before numbers inputted
+        firstNum = "-";
+        display.textContent = firstNum;
+        console.log(firstNum, answer, operator, secondNum); 
+    } else if (!operator) { //negating first value
+        if (firstNum === "-") {
+            firstNum = "";
+        } else {
+            firstNum = String(-firstNum);
+        }
+        display.textContent = firstNum;
+        console.log(firstNum, answer, operator, secondNum);
+    } else if (!secondNum) { //negating second value before numbers for second value inputted
+        secondNum = "-";
+        display.textContent = secondNum;
+        console.log(secondNum)
+    } else { //negating second value
+        if (secondNum === "-") {
+            secondNum = "";
+        } else {
+            secondNum = String(-secondNum);
+        }
+        display.textContent = secondNum;
+        console.log(secondNum)
+    }
+}
+
